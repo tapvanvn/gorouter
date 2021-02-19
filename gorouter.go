90cb91e041ee796efb4c526792dc5217067577cd
tapvanvn <tapvanvn@gmail.com>
2021-02-19 11:24:54 +0700

    support trail handle

--- gorouter.go
+++ gorouter.go
@@ -24,6 +24,16 @@ type RouteContext struct {
 	R            *http.Request
 }
 
+//Trail pass context to handle in trail of handler
+func (context *RouteContext) Trail(trails []RouteHandle) {
+	for _, handler := range trails {
+		if context.Handled {
+			return
+		}
+		handler(context)
+	}
+}
+
 //AnyIndex find if any index with name in current stack of context
 func (context *RouteContext) AnyIndex(name string) (string, bool) {
 
