2b8719c54e6cc47ecd52703112b7415af07fa7cb
Duy Nguyen <tapvanvn@gmail.com>
2022-05-01 01:48:42 +0700

    update

--- endpoint.go
+++ endpoint.go
@@ -1,7 +1,20 @@
 package gorouter
 
+import "strings"
+
 //EndpointDefine ...
 type EndpointDefine struct {
 	Measurement bool
 	Handles     []RouteHandle
+}
+
+func GetBlueprint(endpointDefine string) string {
+	parts := strings.Split(endpointDefine, "/")
+	remain := []string{}
+	for _, part := range parts {
+		if part[0] != ':' {
+			remain = append(remain, part)
+		}
+	}
+	return strings.Join(remain, "/")
 }
