cd1a4d7ec4d495b45bd0c4bba48432ea08b6ad72
Duy Nguyen <tapvanvn@gmail.com>
2022-08-02 22:24:34 +0700

    update

--- api_form.go
+++ api_form.go
@@ -3,6 +3,7 @@ package gorouter
 import (
 	"bytes"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -50,8 +51,18 @@ type ApiForm struct {
 }
 
 func (frm *ApiForm) Request(domain string, path string, indexes map[string]interface{}) (*ApiResponse, error) {
-	url := strings.TrimSuffix(domain, "/") + "/" + strings.TrimPrefix(path, "/")
 
+	params := url.Values{}
+	//paramSegments := []string{}
+	for key, value := range frm.Params {
+		params.Add(key, value)
+		//paramSegments = append(paramSegments, fmt.Sprintf("%s=%s", ))
+	}
+
+	urlStr := strings.TrimSuffix(domain, "/") + "/" + strings.TrimPrefix(path, "/")
+	if len(frm.Params) > 0 {
+		urlStr += "?" + params.Encode()
+	}
 	var req *http.Request = nil
 	var err error = nil
 	if frm.Method == MethodPost || frm.Method == MethodPut {
@@ -64,18 +75,20 @@ func (frm *ApiForm) Request(domain string, path string, indexes map[string]inter
 			//TODO: build json
 			break
 		}
-		req, err = http.NewRequest(string(frm.Method), url, bytes.NewBuffer([]byte(bodyData)))
+		req, err = http.NewRequest(string(frm.Method), urlStr, bytes.NewBuffer([]byte(bodyData)))
 		if frm.ContentType != "" {
 			req.Header.Set("Content-Type", string(frm.ContentType))
 		}
 		defer req.Body.Close()
 	} else {
-		req, err = http.NewRequest(string(frm.Method), url, nil)
+		req, err = http.NewRequest(string(frm.Method), urlStr, nil)
 	}
 	if err != nil {
 		return nil, err
 	}
-
+	for key, value := range frm.Headers {
+		req.Header.Set(key, value)
+	}
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
